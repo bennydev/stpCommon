68e84ec3ffbc3d3b809d7c1716f97b1a08fef89b
Johnny Nilsson <Johnny.Nilsson@folksam.se>
2015-12-11 13:40:32 +0100

    Pre-release of version 0.2.5 Now with possibility to overwrite the navigation text key

--- dist/stpNavigation.js
+++ dist/stpNavigation.js
@@ -11,6 +11,6 @@ angular.module('stpNavigationTemplates', ['navigation/navigation.tpl.html']);
 angular.module("navigation/navigation.tpl.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("navigation/navigation.tpl.html",
     "<div class=\"u-align-center u-bgcolor-blue-4\">\n" +
-    "    <button id=\"navButton\" class=\"button button--primary u-spacing-above u-spacing-under\" ng-click=\"next();\" translate translate-values=\"{summaryStatus: sections.summary.status}\">VIEW.NAVIGATION.NEXT</button>\n" +
+    "    <button id=\"navButton\" class=\"button button--primary u-spacing-above u-spacing-under\" ng-click=\"next();\">{{navigationTextKey || 'VIEW.NAVIGATION.NEXT' | translate}}</button>\n" +
     "</div>");
 }]);
