b7ff77ae447dac98f4057518122c5f8ce5e67512
Johnny Nilsson <Johnny.Nilsson@folksam.se>
2015-12-11 16:07:57 +0100

    Pre-release of version 0.3.3 built dist for summary

--- dist/stpSummary.js
+++ dist/stpSummary.js
@@ -63,6 +63,7 @@ angular.module('stpCommon.summary',
         function next(){}
     }])
     .controller('SummaryCtrl', ['$scope', 'HeaderService', 'SummaryService', function($scope, HeaderService, SummaryService){
+        $scope.navigationTextKey = 'VIEW.SECTIONS.SUMMARY.NAVIGATION.NEXT';
         $scope.customer = {fullName: HeaderService.getCustomerFullName()};
         $scope.policyHolder = {fullName: HeaderService.getPolicyHolderFullName()};
         $scope.goTo = SummaryService.goTo;
