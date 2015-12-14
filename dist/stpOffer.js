dec5cb406dda716a33e59689de79b077425f0468
Johnny Nilsson <Johnny.Nilsson@folksam.se>
2015-12-14 11:14:27 +0100

    Pre-release of version 0.4.1 added offer dist

---
+++ dist/stpOffer.js
@@ -0,0 +1,66 @@
+"use strict";
+angular.module('stpCommon.offer', [])
+    .controller('OfferCtrl', ['$scope', 'OfferService', function($scope, OfferService){
+        $scope.template = OfferService.getClaimType() === 'LTP' ? 'offer/ltp/ltp.tpl.html' : 'offer/stp/stp.tpl.html';
+        $scope.claimId = OfferService.getClaimId();
+    }])
+    .factory('OfferService', [function(){
+        var service = {
+            getOfferModel: getOfferModel
+        };
+        return service;
+
+        function getOfferModel(){
+            var model = {
+                getClaimType: getClaimId,
+                getClaimId: getClaimId
+            };
+            return model;
+
+            function getClaimType(){
+                return 'LTP';
+            }
+
+            function getClaimId(){
+                return 'FF123456789S'
+            }
+        }
+    }]);
+angular.module('stpOfferTemplates', ['offer/ltp/ltp.tpl.html', 'offer/ltp/thanks.tpl.html', 'offer/offer.tpl.html']);
+
+angular.module("offer/ltp/ltp.tpl.html", []).run(["$templateCache", function($templateCache) {
+  $templateCache.put("offer/ltp/ltp.tpl.html",
+    "<div ng-include=\"'offer/ltp/thanks.tpl.html'\"></div>");
+}]);
+
+angular.module("offer/ltp/thanks.tpl.html", []).run(["$templateCache", function($templateCache) {
+  $templateCache.put("offer/ltp/thanks.tpl.html",
+    "<div class=\"u-bgcolor-blue-5 u-small-type\">\n" +
+    "    <div class=\"grid-wrapper\">\n" +
+    "        <div class=\"u-padding-above\">\n" +
+    "\n" +
+    "            <h3 class=\"u-typography-2 u-spacing-under-narrow\" translate>VIEW.OFFER.LTP.THANKS.HEADER</h3>\n" +
+    "\n" +
+    "            <div class=\"grid grid--wide\">\n" +
+    "                <div class=\"grid__item sm--one-half\">\n" +
+    "                    <p translate translate-values=\"{claimId: claimId}\">VIEW.OFFER.LTP.THANKS.INFO_CLAIM_ID</p>\n" +
+    "\n" +
+    "                    <div class=\"u-spacing-above\">\n" +
+    "                        <p translate translate-values=\"{eventType: offerModel.eventType}\">VIEW.OFFER.LTP.THANKS.INFO_CONTACT</p>\n" +
+    "                        <p translate>VIEW.OFFER.REGARDS</p>\n" +
+    "                        <p translate>VIEW.OFFER.FOLKSAM</p>\n" +
+    "                    </div>\n" +
+    "\n" +
+    "                </div>\n" +
+    "            </div>\n" +
+    "        </div>\n" +
+    "    </div>\n" +
+    "</div>");
+}]);
+
+angular.module("offer/offer.tpl.html", []).run(["$templateCache", function($templateCache) {
+  $templateCache.put("offer/offer.tpl.html",
+    "<div ng-controller=\"OfferCtrl\">\n" +
+    "    <ng-include src=\"template\"></ng-include>\n" +
+    "</div>");
+}]);
