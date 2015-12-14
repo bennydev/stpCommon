3d71bdee551e2d172031e15f1a5e6015b055f95f
Johnny Nilsson <Johnny.Nilsson@folksam.se>
2015-12-14 11:20:17 +0100

    Pre-release of version 0.4.2 bug fix

--- dist/stpOffer.js
+++ dist/stpOffer.js
@@ -1,8 +1,8 @@
 "use strict";
 angular.module('stpCommon.offer', [])
     .controller('OfferCtrl', ['$scope', 'OfferService', function($scope, OfferService){
-        $scope.template = OfferService.getClaimType() === 'LTP' ? 'offer/ltp/ltp.tpl.html' : 'offer/stp/stp.tpl.html';
-        $scope.claimId = OfferService.getClaimId();
+        $scope.template = OfferService.getOfferModel().getClaimType() === 'LTP' ? 'offer/ltp/ltp.tpl.html' : 'offer/stp/stp.tpl.html';
+        $scope.claimId = OfferService.getOfferModel().getClaimId();
     }])
     .factory('OfferService', [function(){
         var service = {
