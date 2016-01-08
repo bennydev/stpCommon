ee736362cec9ad8acf0bb0c034c361f9035e1863
Benny Henriksson Jarnroth <Benny.Henriksson@cag.se>
2016-01-08 16:39:07 +0100

    Fixed bug with undefined names.

--- src/header/LeadingCapitalAndLowerCase.js
+++ src/header/LeadingCapitalAndLowerCase.js
@@ -2,8 +2,11 @@
 angular.module('stpCommon.header').filter('capitalAndLowerCase', function() {
 
     return function (str){
-        return str.replace(/[^-'\s]+/g, function(txt){
-            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-        });
+        if (str !== undefined) {
+            return str.replace(/[^-'\s]+/g, function (txt) {
+                return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+            });
+        }
+
     };
 });
