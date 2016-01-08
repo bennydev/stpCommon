00efd5a5de681b092f35b96b561dc5f7e292605f
Johnny Nilsson <Johnny.Nilsson@folksam.se>
2016-01-08 16:56:03 +0100

    adding renamed file

---
+++ src/header/leadingCapitalAndLowerCase.spec.js
@@ -0,0 +1,27 @@
+"use strict";
+describe('format filter tests.', function () {
+
+
+    beforeEach(module('stpCommon.header'));
+
+    it('should format first name properly', inject(function (capitalAndLowerCaseFilter) {
+        var name = 'CARL-JAN';
+        var result = capitalAndLowerCaseFilter(name);
+        expect(result).toBe('Carl-Jan');
+    }));
+    it('should format last name properly', inject(function (capitalAndLowerCaseFilter) {
+        var name = 'AXELSSON JOHNSON';
+        var result = capitalAndLowerCaseFilter(name);
+        expect(result).toBe('Axelsson Johnson');
+    }));
+    it('should not format empty name properly', inject(function (capitalAndLowerCaseFilter) {
+        var name = '';
+        var result = capitalAndLowerCaseFilter(name);
+        expect(result).toBe('');
+    }));
+    it('should not format undefined name', inject(function (capitalAndLowerCaseFilter) {
+        var result = capitalAndLowerCaseFilter();
+        expect(result).toBeUndefined();
+    }));
+
+});
