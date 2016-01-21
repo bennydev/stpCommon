0aaccf0a7878e95eab81a468262e04f5b5084cc8
Benny Henriksson Jarnroth <Benny.Henriksson@cag.se>
2016-01-21 12:22:53 +0100

    Further work on mobile phone part.

--- dist/stpContact.js
+++ dist/stpContact.js
@@ -15,7 +15,7 @@ angular.module('stpCommon.contact', ['fsmQuestion'])
         function getQuestionGroups(){
             createQuestions();
             var groups = [];
-            groups.push(createGroup([QuestionService.getQuestion('contactCountry'), QuestionService.getQuestion('contactPhone')]));
+            groups.push(createGroup([QuestionService.getQuestion('contactPhone')]));
             groups.push(createGroup([QuestionService.getQuestion('contactEmail')]));
             QuestionService.getQuestion('contactCountry').options = CountryCodeService.getCountryCodes();
             return groups;
@@ -27,33 +27,58 @@ angular.module('stpCommon.contact', ['fsmQuestion'])
 
         function createQuestions(){
             if (!questionsCreated) {
-                contactCountry();
+                //contactCountry();
                 contactPhone();
                 contactEmail();
                 questionsCreated = true;
             }
         }
 
-        function contactCountry() {
-            return QBuilder
-                .id('contactCountry')
-                .type(QuestionTypes.select)
-                .text({ root: 'VIEW.SECTIONS.EVENT.CONTACT_INFO.QUESTIONS.CONTACT_PHONE'})
-                .required(false)
-                .createQuestion();
-        }
+        //function contactCountry() {
+        //    return QBuilder
+        //        .id('contactCountry')
+        //        .type(QuestionTypes.select)
+        //        .text({ root: 'VIEW.SECTIONS.EVENT.CONTACT_INFO.QUESTIONS.CONTACT_PHONE'})
+        //        .required(false)
+        //        .createQuestion();
+        //}
 
         function contactPhone() {
+            function isSwedenAndZeroOrOtherAndBlank(question){
+                var answer = question.answer;
+                return (answer.countryCode.code === 'SWE' && answer.phoneNumber === '0') ||
+                    (answer.countryCode.code !== 'SWE' && answer.phoneNumber === '');
+            }
+            var phoneValidator = {
+                validate: function(question){
+                    var result = {valid: true, cause: 'format', message: question.textRoot+'.ERRORS.FORMAT'};
+                    if(!isSwedenAndZeroOrOtherAndBlank(question)) {
+                        result.valid = question.getAnswer().phoneNumber.toString().removeChars(['-', ' ', '.']).isNumeric();
+                    }
+                    return result;
+                }
+            };
+
             return QBuilder
                 .id('contactPhone')
-                .type(QuestionTypes.input)
+                .type(QuestionTypes.phone)
                 .text({ root:'VIEW.SECTIONS.EVENT.CONTACT_INFO.QUESTIONS.CONTACT_PHONE'})
                 .max(30)
                 .required(false)
+                .validator(phoneValidator)
                 .createQuestion();
         }
 
         function contactEmail() {
+            var emailValidator = {
+                validate: function(question){
+                    var emailRegex = /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/;
+                    var result = {valid: true, cause: 'format', message: question.textRoot+'.ERRORS.FORMAT'};
+                    result.valid = emailRegex.test(question.answer);
+                    return result;
+                }
+            };
+
             return QBuilder
                 .id('contactEmail')
                 .type(QuestionTypes.input)
@@ -61,6 +86,7 @@ angular.module('stpCommon.contact', ['fsmQuestion'])
                 .placeholder('ditt.namn@exempel.se')
                 .max(254)
                 .required(true)
+                .validator(emailValidator)
                 .createQuestion();
         }
 
