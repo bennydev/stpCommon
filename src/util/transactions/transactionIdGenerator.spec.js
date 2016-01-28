e6f6176ad020e9b3515ec199aac4e8b8c1464676
Benny Henriksson Jarnroth <Benny.Henriksson@cag.se>
2016-01-28 11:41:20 +0100

    Added test.

--- src/util/transactions/transactionIdGenerator.spec.js
+++ src/util/transactions/transactionIdGenerator.spec.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 describe('Cyclic redundancy check (CRC) tests.', function() {
     var utils;
     beforeEach(module('stpCommon.util'));
@@ -45,11 +45,19 @@ describe('Cyclic redundancy check (CRC) tests.', function() {
         var crc2 = utils.transactionIdGenerator.crc32FromObject(getCustomerObject());
         expect(crc).not.toEqual(crc2);
     });
-    it ('should be able to generate different transaction ids for two objects with different names', function() {
+    it ('should be able to generate different transaction ids for two customer objects with different names', function() {
         var customer = getCustomerObject();
         customer.name = 'Tolvan Eolvansson';
         var crc = utils.transactionIdGenerator.crc32FromObject(customer);
         var crc2 = utils.transactionIdGenerator.crc32FromObject(getCustomerObject());
+        expect(crc).not.toEqual(crc2);
+    });
+
+    it ('should be able to generate different transaction ids for two save claim objects with different values', function() {
+        var saveClaim = getSaveClaimObject();
+        saveClaim.customer.pesronId = '121212121212';
+        var crc = utils.transactionIdGenerator.crc32FromObject(saveClaim);
+        var crc2 = utils.transactionIdGenerator.crc32FromObject(getSaveClaimObject());
         expect(crc).not.toEqual(crc2);
     });
 
@@ -58,5 +66,55 @@ describe('Cyclic redundancy check (CRC) tests.', function() {
         obj.id = '121212121212';
         obj.name = 'Tolvan Tolvansson';
         return obj;
+    }
+    
+    function getSaveClaimObject() {
+        return {
+            customer : {
+                personId : '201212121212',
+                holdsPolicy : 'YES',
+                email : 'bennyhenriksson@gmail.com'
+            },
+            claimDetail : {
+                insideLockedArea : false,
+                locationType : 'SWEDEN',
+                policeReportExist : false,
+                extraEquipmentList : [ {
+                    description : 'Lås',
+                    priceToday : '250',
+                    estimatedAge : 'RANGE_6_MONTHS_TO_1_YEAR'
+                } ],
+                dateOfLoss : '2016-01-25T00:00:00'
+            },
+            claimObjects : [ {
+                lossType : 'STOLEN',
+                itemType : 'BICYCLE',
+                objectLocked : true,
+                product : {
+                    attributes : [ {
+                        key : 'id',
+                        keyDescription : 'Atala',
+                        value : '63817'
+                    }, {
+                        key : '000',
+                        keyDescription : 'Beskriv cykeln',
+                        value : 'asdfadadasdasd'
+                    } ],
+                    valueLink : '/valuation/product/52284'
+                },
+                productFound : true,
+                purchaseNew : {
+                    customerEstimatedPrice : '1234',
+                    placeOfPurchase : 'asdasdasdasdasd',
+                    date : 2012-12-12
+                },
+                receiptExist : true,
+                majorDamagesBeforeEvent : false,
+                repairmanContacted : false,
+                estimateReparationCost : '0',
+                ownerType : 'SELF'
+            } ],
+            claimRequestTransactionId : ''
+        };
     }
 });
