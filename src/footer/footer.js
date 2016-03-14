6d8b0802aa88869dd505637adb254ccc1b9d908a
bennydev <benny.henriksson@cag.se>
2016-03-14 14:47:07 +0100

    Added timeout to applicationVersion call.

--- src/footer/footer.js
+++ src/footer/footer.js
@@ -4,13 +4,14 @@
     angular.module('stpCommon.footer', ['stpCommon.util'])
         .controller('FooterCtrl', FooterController);
 
-    FooterController.$inject = ['$scope', '$log', 'StpUtils'];
+    FooterController.$inject = ['$scope', '$log', '$http'];
 
     function FooterController($scope, $log, StpUtils) {
+        var httpConfig = {timeout: 10000};
         $scope.environments = StpUtils.environmentService.getEnvironments();
         $scope.currentEnvironment = StpUtils.environmentService.getCurrentEnvironment();
         function getApplicationVersion() {
-            StpUtils.versionRestService.applicationVersion(function (data) {
+            $http.get('resources/system/applicationVersion', httpConfig).then(function (data) {
                 $scope.name = data.name;
                 $scope.version = data.version;
                 $scope.environment = data.environment;
