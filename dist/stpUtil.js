0dc04396d79c6d0693437bd3047e8e7f54569b18
bennydev <benny.henriksson@cag.se>
2016-03-14 14:21:42 +0100

    Added timeout to applicationVersion call.

--- dist/stpUtil.js
+++ dist/stpUtil.js
@@ -168,11 +168,12 @@ angular.module('stpCommon.util')
     'use strict';
 
     angular.module('stpCommon.util').factory('VersionRestService', VersionRestService);
-
+    var httpConfig = {timeout: 10000};
     VersionRestService.$inject = ['$resource'];
 
     function VersionRestService($resource) {
-        return $resource('resources', null, {
+
+        return $resource('resources', httpConfig, {
             'applicationVersion': {
                 method: 'get',
                 url: 'resources/system/applicationVersion',
--- src/util/restServices/versionRestService.js
+++ src/util/restServices/versionRestService.js
@@ -2,11 +2,12 @@
     'use strict';
 
     angular.module('stpCommon.util').factory('VersionRestService', VersionRestService);
-
+    var httpConfig = {timeout: 10000};
     VersionRestService.$inject = ['$resource'];
 
     function VersionRestService($resource) {
-        return $resource('resources', null, {
+
+        return $resource('resources', httpConfig, {
             'applicationVersion': {
                 method: 'get',
                 url: 'resources/system/applicationVersion',
