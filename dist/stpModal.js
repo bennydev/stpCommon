5a314977b3be7fbc4de8e7f6c205be253ead2267
spiffen <martin.lindhe@folksam.se>
2016-04-04 17:20:00 +0200

    reverted "test av modal knappfocus"

--- dist/stpModal.js
+++ dist/stpModal.js
@@ -34,16 +34,10 @@ angular.module('stpCommon.modal', ['stpCommon.util'])
         $scope.templateLoaded = function(){
             var elements = $('.modal__content--dialog .button');
             var elementsLength = elements.length;
-            for(var i=0; i<elementsLength; i++){elements[i].tabIndex=i+1;}
-
-            elements[elementsLength-1].addEventListener('keydown', function(event){
-                    var firstElement = $('.modal__content--dialog .button').eq(0);
-                    var k = event.which || event.keyCode;
-                    if(k===9) {
-                        firstElement[0].focus();
-                    }
-                });
-              elements[0].focus();
+            for(var i=0; i<elementsLength; i++){
+                elements[i].tabIndex=i+1;
+            }
+            elements[0].focus();
         };
     }]);
 angular.module('stpModalTemplates', ['modal/modal.tpl.html', 'modal/modalError.tpl.html']);
