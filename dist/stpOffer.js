0e0fed66830e2f02dc6ded2b9b1d77938810411e
bennydev <benny.henriksson@cag.se>
2016-04-11 11:17:55 +0200

    Added getStandardTemplate to AbstractOfferService.

--- dist/stpOffer.js
+++ dist/stpOffer.js
@@ -8,7 +8,8 @@ angular.module('stpCommon.offer', [])
     }])
     .factory('AbstractOfferService', [function(){
         var service = {
-            getOfferModel: getOfferModel
+            getOfferModel: getOfferModel,
+            getStandardTemplate: getTemplate
         };
         return service;
 
